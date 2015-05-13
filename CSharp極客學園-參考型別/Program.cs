285e6698f7d7c8f689055ebe669bdad8049f5a42
Chris99252 <chris@ccf.org.tw>
2015-05-13 23:33:35 +0800

    CSharp極客學園：參考型別 - Class

---
+++ CSharp極客學園-參考型別/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp極客學園_參考型別
+{
+    class Program
+    {
+        /// <summary>
+        /// 預定型別 object, string, dynamic
+        /// System.Object, System.String, System.Dynamic
+        /// 編譯階段（大部分的類型檢查），運行階段（Dynamic的類別檢查）
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // System.Object (最基本的型別)
+
+            object o = new object(); // 等價關係
+            object o2 = new Object();
+            object o3 = new System.Object();
+
+            Console.WriteLine(o.GetType()); // 輸出 System.Object
+            Console.WriteLine(o.ToString()); // 輸出 System.Object
+            
+            int i = 5;
+            Console.WriteLine(i.ToString()); // 輸出 5
+
+            // System.String 
+
+            string s = "CSharp極客學園"; // 宣告時，長度已經固定
+            string s2 = "CSharp";
+            s2 += "極客學園";
+            Console.WriteLine(s); // 輸出 CSharp極客學園
+            Console.WriteLine(s2); // 輸出 CSharp極客學園
+            
+            Console.WriteLine(s == s2); // 輸出 True 值相等
+            Console.WriteLine((object)s == (object)s2); // 輸出 False 位址不相等，強制轉換
+            
+            char c = s[2]; // 取得單一字符
+            Console.WriteLine(c); // 輸出 h
+
+            string u = "\\\u0066\n";
+            Console.WriteLine(u); // [ \\ -> \ ], [ \u0066 -> f ], [ \n -> enter ]
+
+            string at = @"C\CSharp極客學園\Program.cs";
+            Console.WriteLine(at); // 輸出 C\CSharp極客學園\Program.cs
+            string noAt = "C\\CSharp極客學園\\Program.cs";
+            Console.WriteLine(noAt); // 輸出 C\CSharp極客學園\Program.cs
+
+            Console.WriteLine(at.Contains("極客")); // 是否包含，輸出 True
+            Console.WriteLine(at.Length); // 字串長度，輸出 23 
+            Console.WriteLine(at.IndexOf("極")); //字符首次在字串出現的位置，輸出 8
+
+            // 因 string 在宣告時就給予固定的長度
+            // 若需要對 string 新增或修改字串長度，需要重新配置記憶體的內存空間，耗資源
+            // 可以改用 StringBuilder
+
+            StringBuilder builder = new StringBuilder(); // 宣告時，長度不固定
+            builder.Append("CSharp");
+            builder.Append("極客學園");
+            Console.WriteLine(builder); // 輸出 CSharp極客學園
+            builder.AppendFormat(" hello{0} {1}", "極客學園", "CSharp"); // 輸出 CSharp極客學園 hello極客學園 CSharp
+            Console.WriteLine(builder);
+        }
+    }
+}
---
+++ CSharp極客學園.參考型別.Class/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp極客學園.參考型別.Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(5);
+
+            // 若為宣告存取修飾詞，預設是 internal p.GetAge() 不存在
+
+            Console.WriteLine(person.GetAge()); // 回傳 age，輸出 5
+
+            Console.WriteLine(Person.GetFive()); // 輸出 5
+
+            // 靜態方法與靜態成員，都只存在於自己的類別內，不是在實例化變數，所以 person.GetFive() 不存在
+
+            Console.WriteLine(person.Age); // 輸出 0，有 get，可以取得值，int 默認值為零
+
+            person.Age = 10; // 因為有 set ，可以賦值
+
+            Console.WriteLine(person.GetAge());
+
+            Console.WriteLine(person.Age2); // 輸出 15，age 初始給予值 5，person.Age （get）設定 5 + 10
+
+
+        }
+    }
+    class Person
+    {
+        // 資料成員 data member
+
+        int age;
+
+        // 屬性 Property
+
+        public int Age { get; set; } //  age 為 private 資料成員，Property 提供一個可以讓外部使用 age 的方式
+
+        public int Age2
+        { 
+            get
+            {
+                return age + 10;
+            }
+            set
+            {
+                age = value - 10;
+            }
+        }
+
+        // 建構函式 Constructor
+
+        public Person(int myAge)  
+        {
+            // 建構式
+            this.age = myAge; // this 整個 Person class 內的 age
+        }
+
+        // 方法 Method
+
+        public int GetAge() // 存取修飾詞 internal，內部的，限於目前的檔案中
+        {
+            //string result = "5";
+            //return result; // 無法將型別 'string' 隱含轉換為 'int'
+
+            return age; // 回傳的型別與宣告的Method型別要一致
+        }
+
+        public static int GetFive() // 靜態方法
+        {
+            return 5;
+        }
+    }
+}
