edaed8d87a59c9c834945dd93a889d6c40de171a
Chris99252 <chris@ccf.org.tw>
2015-05-15 23:25:22 +0800

    CSharp極客學園 - 型別轉換：boxing 與 unboxing 與 Nullable

--- CSharp極客學園.型別轉換/Program.cs
+++ CSharp極客學園.型別轉換/Program.cs
@@ -10,6 +10,9 @@ namespace CSharp極客學園.型別轉換
     /// C# 是強型別的語言，在編譯的時候就會進行型別的檢查，避免在運行的時候出錯
     /// 類型轉換：隱式轉換(自動轉換)（向上轉換） 與 顯式轉換（強制轉換）（向下轉換）
     /// 透過內建提供的Method方法：ToString(), Convert.ToInt32, Int32.Parse, Int32.TryParse
+    /// boxing 裝箱：把一個實值型別，轉為參考型別
+    /// unboxing 拆箱：把一個參考型別，轉為實值型別
+    /// boxing 與 unboxing，都在記憶體上做搬移，很耗費資源，盡可能避免
     /// </summary>
     class Program
     {
@@ -25,7 +28,7 @@ namespace CSharp極客學園.型別轉換
 
             double d = 10.05;
             int iFormD = (int)d;
-            Console.WriteLine(iFormD); // 輸出 10，資料會有缺失的風險
+            Console.WriteLine(iFormD); // 輸出 10，資料會有缺失的風險，以及例外錯誤
             C1 c11 = new C1();
             try
             {
@@ -42,7 +45,7 @@ namespace CSharp極客學園.型別轉換
             Console.WriteLine(c11 is C1); // 輸出 True
             Console.WriteLine(c11 is C2); // 輸出 False
 
-            // 利用as，安全的方式進行轉換
+            // 利用 as，安全的方式進行轉換
 
             C2 c22 = c11 as C2; // 可運行不會出錯，c11在轉換的過程中發現不能轉為 C2 型別，會丟回一個 null 值
 
@@ -71,6 +74,39 @@ namespace CSharp極客學園.型別轉換
             Console.WriteLine(succeed2 + " : " + iFromS5); // 輸出 False : 0，轉換失敗，輸出整數的空值 0
 
             // 實作自己的轉換，透過 IConvertible, TypeConverter
+
+            int iToBoxing = 100;
+
+            object iBoxed = iToBoxing; // boxing 隱式轉換，因為所有的型別都是繼承 System.object，向上轉換
+
+            int iUnBoxing = (int)iBoxed; // unBoxing 顯式轉換，由父類別轉為子類別，向下轉型
+
+            object nullObject = null;
+
+            // int iNull = (int)nullObject; // 錯誤：NullReferenceException。
+
+            int? iNullable = null; // 宣告一個可為 null 值的 int
+
+            System.Nullable<int> iNullable2 = 100; // 等同於 int?
+
+            Console.WriteLine(iNullable2.Value); // 輸出 100
+
+            // Console.WriteLine(iNullable.Value); // 錯誤：可為 Null 的物件必須具有值
+
+            Console.WriteLine(iNullable.HasValue); // 輸出：false，可以預先判斷
+
+            Console.WriteLine(iNullable.GetValueOrDefault()); // 輸出：0，若為空值，給予預設的 null 值
+
+            Console.WriteLine(iNullable2.GetValueOrDefault()); // 輸出：100
+
+            int i2 = iNullable ?? 500; // 若 ?? 前的變數值為 null，那就給予 ?? 後面的值
+
+            Console.WriteLine(i2); // 輸出：500
+
+            int i3 = iNullable2 ?? 500; // 若 ?? 前的變數值為 null，那就給予 ?? 後面的值
+
+            Console.WriteLine(i3); // 輸出 100
+
 
         }
     }
