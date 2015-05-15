0547606b9ce04f04cfe1bb3a58b6ceeb234bb7d3
Chris99252 <chris@ccf.org.tw>
2015-05-15 22:53:33 +0800

    CSharp極客學園 - 型別轉換：透過Method進行轉換

--- CSharp極客學園.型別轉換/Program.cs
+++ CSharp極客學園.型別轉換/Program.cs
@@ -9,6 +9,7 @@ namespace CSharp極客學園.型別轉換
     /// <summary>
     /// C# 是強型別的語言，在編譯的時候就會進行型別的檢查，避免在運行的時候出錯
     /// 類型轉換：隱式轉換(自動轉換)（向上轉換） 與 顯式轉換（強制轉換）（向下轉換）
+    /// 透過內建提供的Method方法：ToString(), Convert.ToInt32, Int32.Parse, Int32.TryParse
     /// </summary>
     class Program
     {
@@ -48,6 +49,29 @@ namespace CSharp極客學園.型別轉換
             Console.WriteLine(c22 == null); // 輸出 True
 
             // int iAS = 5 as int; // as 只能用於參考型別，因為實值型別，不能給予 null 值。
+
+            string sFromi = i.ToString(); // ToString() 轉換成字串
+
+            int iFromS = Convert.ToInt32("100"); // Convert 轉換型別
+
+            //int iFromS2 = Convert.ToInt32("CSharp"); // 錯誤：輸入字串格式不正確。
+
+            int iFromS3 = Int32.Parse("101"); // Parse 轉換型別
+
+            //int iFromS4 = Int32.Parse("CSharp"); // 錯誤：輸入字串格式不正確。
+
+            int iFromS5;
+
+            bool succeed = Int32.TryParse("102", out iFromS5); // TryParse 試著去轉換
+
+            Console.WriteLine(succeed +" : "+iFromS5); // 輸出 True : 102，轉換成功
+
+            bool succeed2 = Int32.TryParse("CSharp", out iFromS5); 
+
+            Console.WriteLine(succeed2 + " : " + iFromS5); // 輸出 False : 0，轉換失敗，輸出整數的空值 0
+
+            // 實作自己的轉換，透過 IConvertible, TypeConverter
+
         }
     }
 
