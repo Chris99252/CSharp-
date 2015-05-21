3a484fc8c646acfea06a6441ed878388494e115f
Chris99252 <chris@ccf.org.tw>
2015-05-21 10:55:45 +0800

    CSharp極客學園：物件導向 - 動態多型

--- CSharp極客學園.物件導向.多型/Program.cs
+++ CSharp極客學園.物件導向.多型/Program.cs
@@ -10,12 +10,14 @@ namespace CSharp極客學園.物件導向.多型
     {
         /// <summary>
         /// 多型：一個型態能夠執行多個功能。
-        /// 1. 靜態多型：編譯時就實現多型
-        /// 2. 動態多型：運行時才實現多型
+        /// 1. 靜態多型：編譯時就實現多型，利用參數的 overload 方法實現。
+        /// 2. 動態多型：運行時才實現多型，利用繼承後 override 方法實現。
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // 靜態多型
+
             PrintHello(); // 會有語法提示，可以使用的多型
 
             PrintHello("word");
@@ -27,6 +29,29 @@ namespace CSharp極客學園.物件導向.多型
             c1.Number = 2;
             c2.Number = 3;
             Console.WriteLine((c1 + c2).Number); // 輸出：5。c1.Number + c2.Number
+
+            // 動態多型，解決以下問題
+
+            // some logic
+            // 繼承方法，若有太多種型別
+            //Human h = new Human();
+            //if (h is Man)
+            //{
+            //    h.CleanRoomSlowly();
+            //}
+            //else if (h is Woman){
+            //    h.CleanRoomQuickly();
+            //}
+            // some logic
+
+            Human human1 = new Man();
+            Human human2 = new Woman();
+
+            human1.CleanRoom(); // 輸出：Human CleanRoom room
+            human2.CleanRoom(); // 輸出：Woman CleanRoom room quickly
+
+            Console.WriteLine(c2.ToStringBase()); // 未覆寫，輸出：CSharp極客學園.物件導向.多型.Complex
+            Console.WriteLine(c2.ToString()); // 覆寫，輸出：3
         }
         
         // 靜態多型： 達成方法的 overload
@@ -61,6 +86,39 @@ namespace CSharp極客學園.物件導向.多型
             Complex c = new Complex();
             c.Number = c1.Number + c2.Number;
             return c;
+        }
+
+        public override string ToString() // override ToString()
+        {
+            return Number.ToString();
+        }
+
+        public string ToStringBase() // base ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    class Human
+    {
+        public virtual void CleanRoom(){
+            Console.WriteLine("Human CleanRoom room");
+        }
+    }
+
+    class Man : Human
+    {
+        public override void CleanRoom()
+        {
+            Console.WriteLine("Man CleanRoom room slowly");
+        }
+    }
+
+    class Woman : Human
+    {
+        public override void CleanRoom()
+        {
+            Console.WriteLine("Woman CleanRoom room quickly");
         }
     }
 }
