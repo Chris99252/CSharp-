e5a34ddf635264f27be3096045ae0f0873c1d2b2
Chris99252 <chris@ccf.org.tw>
2015-05-24 13:12:29 +0800

    CSharp極客學園：異常處理 - Exception Class

--- CSharp極客學園.例外處理.ExceptionHandle/Program.cs
+++ CSharp極客學園.例外處理.ExceptionHandle/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,8 @@ using System.Threading.Tasks;
 namespace CSharp極客學園.異常處理.ExceptionHandle
 {
     /// <summary>
-    /// ExceptionHandle:異常處理，避免程式立即 crash
+    /// ExceptionHandle：異常處理，避免程式立即 crash
+    /// Exception Class：Message 異常錯誤訊息。StackTrace：呼叫堆疊，可以追蹤是在那個流程點出錯。TargetSite：那個 Method 導致出錯。
     /// </summary>
     class Program
     {
@@ -29,7 +31,16 @@ namespace CSharp極客學園.異常處理.ExceptionHandle
             }
 
             Console.WriteLine("After try caych");
+
             // throw new NullReferenceException(); // 空引用異常
+
+            var ae = new ArgumentException(); // 所有參數異常
+            var aen = new ArgumentNullException(); // 具體參數異常，為空值異常
+            var aore = new ArgumentOutOfRangeException(); // 具體參數異常，超過範圍異常
+            var dne = new DirectoryNotFoundException(); // System.IO，文件路徑沒有找到異常
+            var fne = new FileNotFoundException(); // 文件沒有找到異常
+            var ioe = new InvalidOperationException(); // 運算異常
+            var nie = new NotImplementedException(); // 未實現異常
         }
     }
 }
