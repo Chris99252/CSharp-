b1fe77650a89db3f527c88aeb20ce0d68496f7c1
Chris99252 <chris@ccf.org.tw>
2015-05-26 19:03:29 +0800

    CSharp極客學園：委派 - 簡單範例

---
+++ CSharp極客學園.委派/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp極客學園.委派
+{
+    // 委派宣告端
+
+    delegate int NumberChange(int n);
+    class Program
+    {
+        /// <summary>
+        /// 委派 Delegate
+        /// </summary>
+        /// <param name="args"></param>
+
+        static int num = 10;
+
+        static void Main(string[] args)
+        {
+            // System.Delegate delegate
+
+            //呼叫端
+
+            NumberChange nc1 = new NumberChange(AddNum);
+            nc1(25);
+            Console.WriteLine("Value of num : {0}",num);
+        }
+
+        // 邏輯端
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+    }
+}
