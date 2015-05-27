e87e80f5a1e2991319622a5654894ab2d39c3b6b
Chris99252 <chris@ccf.org.tw>
2015-05-27 23:00:30 +0800

    CSharp極客學園：事件event - WinForm下介紹

---
+++ CSharp極客學園.事件event/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp極客學園.事件event
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Method
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello Froms");
+        }
+    }
+}
