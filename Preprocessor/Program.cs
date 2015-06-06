244cb0ca2a4d249d4a92214813cf5d069986da04
Chris99252 <chris@ccf.org.tw>
2015-06-06 16:34:50 +0800

    CSharp極客學院：Preprocessor - if

--- Preprocessor/Program.cs
+++ Preprocessor/Program.cs
@@ -1,4 +1,7 @@
-﻿#define DEBUG
+﻿#define DEBUG // 定義為 Debug 模式
+#undef DEBUG // 解除定義
+#define TRACE
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +12,7 @@ namespace CSharp極客學院.Preprocessor
 {
     /// <summary>
     /// 預處理程序：在實際編譯前就預先處理一些資料
+    /// #region, #define, #undef, #if, #elif, #else, #endif
     /// </summary>
     class Program
     {
@@ -19,6 +23,15 @@ namespace CSharp極客學院.Preprocessor
             var s = "Hello 極客學院";
             Console.WriteLine(s);
             #endregion
+
+            // 只有在 Debug 模式下才執行此段代碼
+            #if (DEBUG)
+                Console.WriteLine("Debuging is enabled.");
+            #elif (TRACE)
+                Console.WriteLine("Tracing is enabled.");
+            #else
+                Console.WriteLine("Debuging is not enabled.");
+            #endif
         }
     }
 }
