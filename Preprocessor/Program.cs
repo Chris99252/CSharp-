46ec41292b805dbf4a514a7b719ff1a66f4a7658
Chris99252 <chris@ccf.org.tw>
2015-06-06 16:55:40 +0800

    CSharp極客學院：Preprocessor - pragma

--- Preprocessor/Program.cs
+++ Preprocessor/Program.cs
@@ -1,6 +1,6 @@
 ﻿#define DEBUG // 定義為 Debug 模式
-#undef DEBUG // 解除定義
-#define TRACE
+//t#undef DEBUG // 解除定義
+//#define TRACE
 
 using System;
 using System.Collections.Generic;
@@ -13,7 +13,12 @@ namespace CSharp極客學院.Preprocessor
     /// <summary>
     /// 預處理程序：在實際編譯前就預先處理一些資料
     /// #region, #define, #undef, #if, #elif, #else, #endif
+    /// #warning 拋出警告 #error 拋出異常
+    /// #line 行數 文件名稱
     /// </summary>
+    
+#pragma warning disable 414, 3021
+    [CLSCompliant(false)]
     class Program
     {
         static void Main(string[] args)
@@ -25,13 +30,29 @@ namespace CSharp極客學院.Preprocessor
             #endregion
 
             // 只有在 Debug 模式下才執行此段代碼
-            #if (DEBUG)
+            #if DEBUG
                 Console.WriteLine("Debuging is enabled.");
-            #elif (TRACE)
+            #line 200 "Sepcial"
+            #warning DEBUG is defined (warning)
+            #elif TRACE
                 Console.WriteLine("Tracing is enabled.");
             #else
                 Console.WriteLine("Debuging is not enabled.");
             #endif
+            #line default
+            // #error I'm not happy! (error)
+        }
+    }
+
+#pragma warning restore 3021
+    [CLSCompliant(false)]
+    public class MyClass
+    {
+        int i = 1;
+
+        public static void M()
+        {
+// #pragma checksum "filename" "{guid}" "check bytes"
         }
     }
 }
