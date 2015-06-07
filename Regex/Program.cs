bc391af82acbcd8192b26de74414079187c8114b
Chris99252 <chris@ccf.org.tw>
2015-06-07 14:27:17 +0800

    CShar極客學園：Regualr - 安裝套件

---
+++ Regex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShar極客學園.Regualr
+{
+    /// <summary>
+    /// 擴充功能與更新
+    /// </summary>
+
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "abc";
+            string pattern = "abc";
+            Console.WriteLine(Regex.IsMatch(s1,pattern)); // 輸出：true
+        }
+    }
+}
