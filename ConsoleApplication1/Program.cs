203948aaad79fe49f2606a1b2818928f8e6ef4bb
Chris99252 <chris@ccf.org.tw>
2015-06-27 12:02:34 +0800

    CSharp極客學院：ExtensionMethod - 實作

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,13 @@ namespace CSharp極客學院.ExtensionMethod
     {
         static void Main(string[] args)
         {
+            DemoLinq();
+
+            StringCount();
+        }
+
+        private static void DemoLinq()
+        {
             int[] numbers = { 10, 45, 15, 39, 21, 36 };
 
             // 使用 System.Linq 內的 （擴充方法） OrderBy，也叫語法糖。
@@ -20,6 +27,25 @@ namespace CSharp極客學院.ExtensionMethod
             {
                 Console.WriteLine("{0} ", item);
             }
+        }
+
+        private static void StringCount()
+        {
+            string s = "This is jikexueyuan C# tutorial";
+
+            int i = s.WordCount();
+
+            Console.WriteLine("Word count of s is {0}", i);
+        }
+    }
+
+    // 擴充方法：靜態的類別，靜態的方法，參數對於哪一個類型進行擴充
+
+    public static class StringExtension
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
         }
     }
 }
