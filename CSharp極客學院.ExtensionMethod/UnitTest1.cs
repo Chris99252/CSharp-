175cb42a8e4d302914e68b8b63b7142236363950
Chris99252 <chris@ccf.org.tw>
2015-06-27 11:48:15 +0800

    CSharp極客學院：ExtensionMethod - 基本介紹

---
+++ CSharp極客學院.ExtensionMethod/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp極客學院.ExtensionMethod
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp極客學院.ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 45, 15, 39, 21, 36 };
+
+            // 使用 System.Linq 內的 （擴充方法） OrderBy，也叫語法糖。
+
+            var result = numbers.OrderBy(n => n);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine("{0} ", item);
+            }
+        }
+    }
+}
