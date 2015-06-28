88e6025fd5c3f8bd94eb118a7eaebbf6fff89b18
Chris99252 <chris@ccf.org.tw>
2015-06-28 10:10:36 +0800

    CSharp極客學院：Initializer - Collection

--- CSharp極客學院.Initializer/Program.cs
+++ CSharp極客學院.Initializer/Program.cs
@@ -17,6 +17,8 @@ namespace CSharp極客學院.Initializer
             // 匿名類別（只允許 get）：常使用在LINQ
 
             AnonymousTypesInital();
+
+            CollectionInital();
         }
 
         private static void ObjectInital()
@@ -54,6 +56,37 @@ namespace CSharp極客學院.Initializer
             foreach (var item in queryJoin)
             {
                 Console.WriteLine("{0} {1} {2}", item.FirstName, item.LastName, item.City);
+            }
+        }
+
+        private static void CollectionInital()
+        {
+            var students = new List<StudentName> 
+            {
+                new StudentName {FirstName = "Mei", LastName = "Mei", ID = 100 },
+                new StudentName() {FirstName = "Li", LastName = "Li", ID = 101 },
+                new StudentName("Small", "White") { ID = 102 },
+                null
+            };
+
+            foreach (var item in students)
+            {
+                if (item != null)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+            Dictionary<int, StudentName> studentDic = new Dictionary<int,StudentName>
+            {
+                { 111, new StudentName {FirstName = "Mei", LastName = "Mei", ID = 100 }},
+                { 222, new StudentName() {FirstName = "Li", LastName = "Li", ID = 101 }},
+                { 333, new StudentName("Small", "White") { ID = 102 }}
+            };
+
+            foreach (var item in studentDic)
+            {
+                Console.WriteLine(item.Value.ToString());
             }
         }
     }
